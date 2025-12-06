f6c3e11f9ea8f04a326663d5faba6b6c56b165da
Raksha Pahariya <100781382+RP2025@users.noreply.github.com>
2025-12-07 01:06:03 +0530

    DAY 06

--- DAY06/Trash_Compactor_01.py
+++ DAY06/Trash_Compactor_01.py
@@ -1,1 +1,25 @@
+import math
+def parse_grid(file_path):
+    ops = []
+    grid = []
+    trash_compactor = 0
+    with open(file_path) as f:
+        lines = [line.strip() for line in f if line.strip()]
 
+    ops = lines[-1].split()
+
+    for line in lines[:-1]:
+        grid.append(list(map(int, line.split())))
+    
+    grid = [list(col) for col in zip(*grid)]
+
+    for op, nums in zip(ops, grid):
+        if op == '*':
+            trash_compactor += (math.prod(nums))
+        elif op == '+':
+            trash_compactor += (sum(nums))
+    
+    return trash_compactor
+
+
+parse_grid("DAY06\trash_compactor.txt")
